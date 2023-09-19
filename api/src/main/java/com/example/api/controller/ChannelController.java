fc400036dbaa16b3420df4d575ef1e586d73bacb
mitani1993 <69582233+mitani1993@users.noreply.github.com>
2023-09-20 08:15:45 +0900

    Create ChannelController.java

---
+++ api/src/main/java/com/example/api/controller/ChannelController.java
@@ -0,0 +1,29 @@
+package com.example.api.controller;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.api.domain.channels.model.Channel;
+
+@RestController
+@RequestMapping("/channels")
+@CrossOrigin
+public class ChannelController {
+
+    @PostMapping()
+    public Channel create(@RequestBody Channel channel) {
+        return channel;
+    }
+
+    @GetMapping()
+    public List<Channel> findAll() {
+        return Collections.emptyList();
+    }
+}
