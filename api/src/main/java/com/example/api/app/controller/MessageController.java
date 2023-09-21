6baf47ca9563cf7d113ad37fa15a095205eed402
mitani1993 <fytim55@gmail.com>
2023-09-22 08:19:48 +0900

    メッセージ検索用メソッド作成

--- api/src/main/java/com/example/api/app/controller/MessageController.java
+++ api/src/main/java/com/example/api/app/controller/MessageController.java
@@ -1,10 +1,16 @@
 package com.example.api.app.controller;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.api.app.service.MessageService;
@@ -24,5 +30,13 @@ public class MessageController {
     public Message post(@PathVariable int channelId, @RequestBody Message message) {
         message.setChannelId(channelId);
         return messageService.post(message);
+    }
+
+    @GetMapping
+    public List<Message> find(
+        @PathVariable("channelId") int channelId,
+        @RequestParam("searchWord") Optional<String> searchWord) {
+        // TODO: Service作成後に修正する。
+        return Collections.emptyList();
     }
 }
